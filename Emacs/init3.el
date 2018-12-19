5b3ee057d04335c24b69298de7ecdcad999bd6cf
C.y <yangruihan@vip.qq.com>
2018-12-19 13:17:03 +0800

    add shift+tab

--- Emacs/init3.el
+++ Emacs/init3.el
@@ -141,3 +141,21 @@
              (setq tab-width 4)
              (setq indent-line-function (quote insert-tab))))
 
+
+;; set shift tab remove 4 spaces
+(global-set-key (kbd "<S-tab>") 'un-indent-by-removing-4-spaces)
+(global-set-key (kbd "<backtab>") 'un-indent-by-removing-4-spaces)
+(defun un-indent-by-removing-4-spaces ()
+  "remove 4 spaces from beginning of the line"
+  (interactive)
+  (save-excursion
+    (save-match-data
+      (beginning-of-line)
+      ;; get rid of tabs at beginning of line
+      (when (looking-at "^\\s-+")
+        (untabify (match-beginning 0) (match-end 0)))
+      (when (looking-at "^    ")
+        (replace-match ""))
+      (when (looking-at "^\t")
+        (replace-match "")))))
+
