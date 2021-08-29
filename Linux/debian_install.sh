be3c93adc8bf4cc0ee09a1ed11429dd29ebdb1ff
Ryan <coderyrh9236@gmail.com>
2021-08-29 13:57:31 +0000

    Add install shell script

--- Linux/debian_install.sh
+++ Linux/debian_install.sh
@@ -4,11 +4,15 @@
 # function
 #----------------------
 function LOG() {
-	echo "[#Install Log] $1"
+    echo "[#Install Log] $1"
 }
 
 function ERROR() {
-	echo "[#Install Error] $1"
+    echo "[#Install Error] $1"
+}
+
+function getGithubLatestReleaseVersion() {
+    retval=`curl --silent "https://api.github.com/repos/$1/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")'`
 }
 
 #----------------------
@@ -24,18 +28,18 @@ LOG "apt install done"
 #----------------------
 LOG "install zsh && oh-my-zsh ..."
 if [ -d "$HOME/.oh-my-zsh" ]; then
-	LOG "oh my zsh already exists, reinstall it"
-	rm -rf "$HOME/.oh-my-zsh" 
+    LOG "oh my zsh already exists, reinstall it"
+    rm -rf "$HOME/.oh-my-zsh" 
 fi
 
 if [ -f "$HOME/.zshrc" ]; then
-	LOG ".zshrc already exists, back up to .zshrc.bac"
-	mv "$HOME/.zshrc" "$HOME/.zshrc.bac"
+    LOG ".zshrc already exists, back up to .zshrc.bac"
+    mv "$HOME/.zshrc" "$HOME/.zshrc.bac"
 fi
 
 printf "y" | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 # change zsh theme
-sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="bira"/' ~/.zshrc
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="bira"/' "$HOME/.zshrc"
 chsh -s $(which zsh)
 LOG "install zsh && oh-my-zsh done"
 
@@ -43,16 +47,21 @@ LOG "install zsh && oh-my-zsh done"
 LOG "install zsh plugin ..."
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-sed -i 's/plugins=(/plugins=( zsh-autosuggestions /' ~/.zshrc
+sed -i 's/plugins=(/plugins=( zsh-autosuggestions /' "$HOME/.zshrc"
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-sed -i 's/plugins=(/plugins=( zsh-syntax-highlighting /' ~/.zshrc
+sed -i 's/plugins=(/plugins=( zsh-syntax-highlighting /' "$HOME/.zshrc"
 
 LOG "install zsh plugin done"
 
 #----------------------
 # git config
 #----------------------
+if [ -f "$HOME/.gitconfig" ]; then
+    LOG "git config file exists, back up to .gitconfig.bac"
+    mv "$HOME/.gitconfig" "$HOME/.gitconfig.bac"
+fi
+
 git config --global alias.st status
 git config --global alias.co checkout
 git config --global alias.ci commit
@@ -63,9 +72,9 @@ git config --global alias.br branch
 #----------------------
 LOG "create script folder ..."
 if [ ! -d "$HOME/.rscripts" ]; then
-	mkdir "$HOME/.rscripts"
+    mkdir "$HOME/.rscripts"
 fi
-echo export PATH='$PATH:$HOME/.rscripts' >> ~/.zshrc
+echo export PATH='$PATH:$HOME/.rscripts' >> "$HOME/.zshrc"
 LOG "create script folder done"
 
 #----------------------
@@ -80,15 +89,16 @@ LOG "config emacs ..."
 
 wget https://raw.githubusercontent.com/yangruihan/Notes/master/Emacs/__emacs2 -O ~/.emacs
 if [ ! -d "$HOME/.emacs.d" ]; then
-	mkdir "$HOME/.emacs.d"
+    mkdir "$HOME/.emacs.d"
 fi
 wget https://raw.githubusercontent.com/yangruihan/Notes/master/Emacs/init9.el -O ~/.emacs.d/init.el
-echo alias emsd=\'emacs --daemon\' >> ~/.zshrc
-echo alias emsdq=\'emacsclient --eval \"\(kill-emacs\)\"\' >> ~/.zshrc
-echo alias emc=\'emacsclient -t -a \"\"\' >> ~/.zshrc
+echo alias emsd=\'emacs --daemon\' >> "$HOME/.zshrc"
+echo alias emsdq=\'emacsclient --eval \"\(kill-emacs\)\"\' >> "$HOME/.zshrc"
+echo alias emc=\'emacsclient -t -a \"\"\' >> "$HOME/.zshrc"
 
 wget https://raw.githubusercontent.com/yangruihan/Notes/master/Emacs/em.py -O ~/.rscripts/em.py
-echo alias em=\'~/.rscripts/em.py\' >> ~/.zshrc
+chmod +x ~/.rscripts/em.py 
+echo alias em=\'~/.rscripts/em.py\' >> "$HOME/.zshrc"
 
 LOG "config emacs done"
 
@@ -98,8 +108,8 @@ LOG "config emacs done"
 LOG "config tmux ..."
 
 if [ -f "$HOME/.tmux.conf" ]; then
-	LOG "tmux conf already exists, make a backup to .tmux.conf.bac"
-	mv "$HOME/.tmux.conf" "$HOME/.tmux.conf.bac"
+    LOG "tmux conf already exists, make a backup to .tmux.conf.bac"
+    mv "$HOME/.tmux.conf" "$HOME/.tmux.conf.bac"
 fi
 
 touch "$HOME/.tmux.conf"
@@ -111,6 +121,90 @@ echo alias tmux=\"tmux -u\" >> "$HOME/.zshrc"
 
 LOG "config tmux done"
 
+#----------------------
+# install delta
+#----------------------
+LOG "install delta ..."
+
+getGithubLatestReleaseVersion "dandavison/delta"
+delta_version=$retval
+LOG "delta version $delta_version"
+wget "https://github.com/dandavison/delta/releases/download/$delta_version/git-delta_${delta_version}_armhf.deb" -O delta.deb
+sudo dpkg -i delta.deb
+rm delta.deb
+
+read -r -d '' delta_config << EOM
+
+[pager]
+    diff = delta
+    log = delta
+    reflog = delta
+    show = delta
+
+[delta]
+    features = side-by-side line-numbers decorations
+    syntax-theme = Dracula
+    plus-style = syntax "#003800"
+    minus-style = syntax "#3f0001"
+
+[delta "decorations"]
+    commit-decoration-style = bold yellow box ul
+    file-style = bold yellow ul
+    file-decoration-style = none
+    hunk-header-decoration-style = cyan box ul
+
+[delta "line-numbers"]
+    line-numbers-left-style = cyan
+    line-numbers-right-style = cyan
+    line-numbers-minus-style = 124
+    line-numbers-plus-style = 28
+
+[interactive]
+    diffFilter = delta --color-only
+
+EOM
+
+echo "$delta_config" >> "$HOME/.gitconfig"
+
+LOG "install delta done"
+
+#----------------------
+# install fd
+#----------------------
+LOG "install fd ..."
+sudo apt install fd-find -y
+echo alias fd=\"fd-find\" >> "$HOME/.zshrc"
+LOG "install fd done"
+
+#----------------------
+# install ripgrep
+#----------------------
+LOG "install ripgrep ..."
+sudo apt install ripgrep -y
+LOG "install ripgrep done"
+
+#----------------------
+# install fzf
+#----------------------
+LOG "install fzf ..."
+sudo apt install fzf -y
+LOG "install fzf done"
+
+#----------------------
+# install mcfly
+#----------------------
+LOG "install mcfly ..."
+getGithubLatestReleaseVersion "cantino/mcfly"
+mcfly_version=$retval
+LOG "mcfly version $mcfly_version"
+wget "https://github.com/cantino/mcfly/releases/download/$mcfly_version/mcfly-${mcfly_version}-armv7-unknown-linux-gnueabihf.tar.gz" -O mcfly.tar.gz
+tar xvf mcfly.tar.gz
+mv mcfly "$HOME/.rscripts/"
+rm mcfly.tar.gz
+echo 'eval "$(mcfly init zsh)"' >> "$HOME/.zshrc"
+LOG "install mcfly done"
+
+
 LOG "-------------------------------"
 LOG "------- Install Finish --------"
 LOG "-------------------------------"
@@ -118,4 +212,9 @@ LOG "-------------------------------"
 #----------------------
 # change to zsh
 #----------------------
-zsh
+if [ ! -n "$ZSH_VERSION" ]; then
+    LOG "change to zsh"
+    zsh
+else
+    source "$HOME/.zshrc"
+fi
