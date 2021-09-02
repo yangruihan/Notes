8666090f16ad23dcc9ee07a00897b42b661eec93
Ryan <coderyrh9236@gmail.com>
2021-09-02 20:42:54 +0800

    Update em.py

--- Emacs/em.py
+++ Emacs/em.py
@@ -5,17 +5,37 @@ import os
 import uuid
 import fileinput
 
+"""
+Make emacs support for pipline
+
+Example:
+    
+    man printf | em.py -
+
+    em.py test
+"""
+
 args = sys.argv[1:]
+
+# check use pipline?
 if len(args) == 1 and args[0] == '-':
     content = []
+    
+    # read content from stdin
     for line in fileinput.input():
         content.append(line)
     s = ''.join(content)
+    
+    # create temple file
     filename = f'/tmp/emacs_pip_tmp_file_{uuid.uuid4().hex}'
     with open(f'{filename}', 'w') as f:
         f.write(s)
+
+    # open temple file with emacsclient
     os.system(f'emacsclient -t -a "" {filename}')
+    
+    # remove temple file
     os.remove(filename)
-else:
+else: # otherwise simple open file with args
     args = ' '.join(args)
     os.system(f'emacsclient -t -a "" {args}')
