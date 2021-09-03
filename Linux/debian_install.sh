6861e000a9a7cabf31eacf3dc863a9a8318ddaad
Ryan <coderyrh9236@gmail.com>
2021-09-03 22:31:44 +0800

    Update debian_install.sh

--- Linux/debian_install.sh
+++ Linux/debian_install.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-#----------------------
+#-------------------------------------------------------------------------------
 # function
-#----------------------
+#-------------------------------------------------------------------------------
+
 function LOG() {
     echo "[#Install Log] $1"
 }
@@ -11,20 +12,32 @@ function ERROR() {
     echo "[#Install Error] $1"
 }
 
-#-----------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
 # get github release latest version number
+# return value save to release_ver
 # for example:
-#    getGithubLatestReleaseVersion "dandavison/delta" # retval=="v0.8.3"
+#    getGithubLatestReleaseVersion "dandavison/delta" # release_ver=="v0.8.3"
 # code from https://gist.github.com/lukechilds/a83e1d7127b78fef38c2914c4ececc3c
-#-----------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
 function getGithubLatestReleaseVersion() {
-    retval=`curl --silent "https://api.github.com/repos/$1/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")'`
+    release_ver=`curl --silent "https://api.github.com/repos/$1/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")'`
 }
 
-#----------------------
+#-------------------------------------------------------------------------------
+# get system arch
+# return value save to sysarch
+#-------------------------------------------------------------------------------
+function getSysArch() {
+    sysarch=`uname -m`
+}
+
+# get system arch
+getSysArch
+
+#-------------------------------------------------------------------------------
 # apt install
-#----------------------
-# sudo apt update
+#-------------------------------------------------------------------------------
+sudo apt update
 LOG "apt install ..."
 sudo apt install \
      git vim zsh curl wget \
@@ -33,9 +46,9 @@ sudo apt install \
      man gdb build-essential net-tools -y
 LOG "apt install done"
 
-#----------------------
+#-------------------------------------------------------------------------------
 # install oh my zsh
-#----------------------
+#-------------------------------------------------------------------------------
 LOG "install zsh && oh-my-zsh ..."
 if [ -d "$HOME/.oh-my-zsh" ]; then
     LOG "oh my zsh already exists, reinstall it"
@@ -64,9 +77,9 @@ sed -i 's/plugins=(/plugins=( zsh-syntax-highlighting /' "$HOME/.zshrc"
 
 LOG "install zsh plugin done"
 
-#----------------------
+#-------------------------------------------------------------------------------
 # git config
-#----------------------
+#-------------------------------------------------------------------------------
 if [ -f "$HOME/.gitconfig" ]; then
     LOG "git config file exists, back up to .gitconfig.bac"
     mv "$HOME/.gitconfig" "$HOME/.gitconfig.bac"
@@ -77,9 +90,9 @@ git config --global alias.co checkout
 git config --global alias.ci commit
 git config --global alias.br branch
 
-#----------------------
+#-------------------------------------------------------------------------------
 # custom script folder
-#----------------------
+#-------------------------------------------------------------------------------
 LOG "create script folder ..."
 if [ ! -d "$HOME/.rscripts" ]; then
     mkdir "$HOME/.rscripts"
@@ -87,14 +100,14 @@ fi
 echo export PATH='$PATH:$HOME/.rscripts' >> "$HOME/.zshrc"
 LOG "create script folder done"
 
-#----------------------
+#-------------------------------------------------------------------------------
 # tldr update
-#----------------------
+#-------------------------------------------------------------------------------
 tldr --update
 
-#----------------------
+#-------------------------------------------------------------------------------
 # emacs config
-#----------------------
+#-------------------------------------------------------------------------------
 LOG "config emacs ..."
 
 wget https://raw.githubusercontent.com/yangruihan/Notes/master/Emacs/__emacs2 -O ~/.emacs
@@ -112,9 +125,9 @@ echo alias em=\'~/.rscripts/em.py\' >> "$HOME/.zshrc"
 
 LOG "config emacs done"
 
-#----------------------
+#-------------------------------------------------------------------------------
 # tmux config
-#----------------------
+#-------------------------------------------------------------------------------
 LOG "config tmux ..."
 
 if [ -f "$HOME/.tmux.conf" ]; then
@@ -131,104 +144,110 @@ echo alias tmux=\"tmux -u\" >> "$HOME/.zshrc"
 
 LOG "config tmux done"
 
-#----------------------
+#-------------------------------------------------------------------------------
 # install delta
-#----------------------
+#-------------------------------------------------------------------------------
 LOG "install delta ..."
 
 getGithubLatestReleaseVersion "dandavison/delta"
-delta_version=$retval
+delta_version=$release_ver
+delta_arch="amd64"
+if [ "$sysarch" = "x86_64" ]; then
+    delta_arch="amd64"
+else
+    delta_arch="armhf"
+fi
 LOG "delta version $delta_version"
-wget "https://github.com/dandavison/delta/releases/download/$delta_version/git-delta_${delta_version}_armhf.deb" -O delta.deb
+LOG "delta arch $delta_arch"
+wget "https://github.com/dandavison/delta/releases/download/$delta_version/git-delta_${delta_version}_${delta_arch}.deb" -O delta.deb
 sudo dpkg -i delta.deb
 rm delta.deb
 
 read -r -d '' delta_config << EOM
-
 [pager]
     diff = delta
     log = delta
     reflog = delta
     show = delta
-
 [delta]
     features = side-by-side line-numbers decorations
     syntax-theme = Dracula
     plus-style = syntax "#003800"
     minus-style = syntax "#3f0001"
-
 [delta "decorations"]
     commit-decoration-style = bold yellow box ul
     file-style = bold yellow ul
     file-decoration-style = none
     hunk-header-decoration-style = cyan box ul
-
 [delta "line-numbers"]
     line-numbers-left-style = cyan
     line-numbers-right-style = cyan
     line-numbers-minus-style = 124
     line-numbers-plus-style = 28
-
 [interactive]
     diffFilter = delta --color-only
-
 EOM
 
 echo "$delta_config" >> "$HOME/.gitconfig"
 
 LOG "install delta done"
 
-#----------------------
+#-------------------------------------------------------------------------------
 # install fd
-#----------------------
+#-------------------------------------------------------------------------------
 LOG "install fd ..."
 sudo apt install fd-find -y
 echo alias fd=\"fd-find\" >> "$HOME/.zshrc"
 LOG "install fd done"
 
-#----------------------
+#-------------------------------------------------------------------------------
 # install ripgrep
-#----------------------
+#-------------------------------------------------------------------------------
 LOG "install ripgrep ..."
 sudo apt install ripgrep -y
 LOG "install ripgrep done"
 
-#----------------------
+#-------------------------------------------------------------------------------
 # install fzf
-#----------------------
+#-------------------------------------------------------------------------------
 LOG "install fzf ..."
 sudo apt install fzf -y
 LOG "install fzf done"
 
-#----------------------
+#-------------------------------------------------------------------------------
 # install mcfly
-#----------------------
+#-------------------------------------------------------------------------------
 LOG "install mcfly ..."
 getGithubLatestReleaseVersion "cantino/mcfly"
-mcfly_version=$retval
+mcfly_version=$release_ver
+mcfly_arch="x86_64-unknown-linux-musl"
+if [ "$sysarch" = "x86_64" ]; then
+    mcfly_arch="x86_64-unknown-linux-musl"
+else
+    mcfly_arch="armv7-unknown-linux-gnueabihf"
+fi
 LOG "mcfly version $mcfly_version"
-wget "https://github.com/cantino/mcfly/releases/download/$mcfly_version/mcfly-${mcfly_version}-armv7-unknown-linux-gnueabihf.tar.gz" -O mcfly.tar.gz
+LOG "mcfly arch $mcfly_arch"
+wget "https://github.com/cantino/mcfly/releases/download/$mcfly_version/mcfly-${mcfly_version}-${mcfly_arch}.tar.gz" -O mcfly.tar.gz
 tar xvf mcfly.tar.gz
 mv mcfly "$HOME/.rscripts/"
 rm mcfly.tar.gz
 echo 'eval "$(mcfly init zsh)"' >> "$HOME/.zshrc"
 LOG "install mcfly done"
 
-#----------------------
+#-------------------------------------------------------------------------------
 # install autojump
-#----------------------
+#-------------------------------------------------------------------------------
 LOG "install autojump ..."
 sudo apt install autojump -y
 echo ". /usr/share/autojump/autojump.sh" >> "$HOME/.zshrc"
-LOG "install autojump
-
+LOG "install autojump"
 LOG "-------------------------------"
 LOG "------- Install Finish --------"
 LOG "-------------------------------"
-
-#----------------------
+#-------------------------------------------------------------------------------
 # change to zsh
-#----------------------
+#-------------------------------------------------------------------------------
 if [ ! -n "$ZSH_VERSION" ]; then
     LOG "change to zsh"
     zsh
