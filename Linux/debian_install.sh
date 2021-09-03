28692f0c5ea3fcee366d446d017ebea07ad7c4ce
Ryan <coderyrh9236@gmail.com>
2021-09-03 13:59:21 +0000

    update install script

--- Linux/debian_install.sh
+++ Linux/debian_install.sh
@@ -11,6 +11,12 @@ function ERROR() {
     echo "[#Install Error] $1"
 }
 
+#-----------------------------------------------------------------------------------
+# get github release latest version number
+# for example:
+#    getGithubLatestReleaseVersion "dandavison/delta" # retval=="v0.8.3"
+# code from https://gist.github.com/lukechilds/a83e1d7127b78fef38c2914c4ececc3c
+#-----------------------------------------------------------------------------------
 function getGithubLatestReleaseVersion() {
     retval=`curl --silent "https://api.github.com/repos/$1/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")'`
 }
